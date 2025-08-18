295cc5864e512da4dcac3cc783832a09ee14cda2
jyotsnayadagiri <99063521+jyotsnayadagiri@users.noreply.github.com>
2025-08-18 15:20:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = "harness-466107"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "notes-vm"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y docker.io git
+    docker run -d -p 80:5000 docker.io/jyotsnayadagiri/python-ci:latest
+  EOT
+}
+
+output "app_url" {
+  value = "http://${google_compute_instance.vm.network_interface[0].access_config[0].nat_ip}"
+}
