6cac0e1bb25bce22492178038abbf3607779745a
jyotsnayadagiri <99063521+jyotsnayadagiri@users.noreply.github.com>
2025-08-22 10:14:38 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,13 +9,11 @@ output "bucket_url" {
   value       = google_storage_bucket.todo_app_bucket.url
 }
 
-output "service_account_email" {
-  description = "Email of the service account"
-  value       = google_service_account.todo_app_sa.email
-}
+
 
 output "application_url" {
   description = "URL to access the Todo application"
   value       = "http://${module.gcp_infra.vm_instance_ip}:8080"
 }
+
 
